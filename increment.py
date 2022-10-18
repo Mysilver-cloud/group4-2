8237af71e5e4cd82685b161d132e2b280db5c898
anh dang <anh.dang@cloudreach.com>
2022-10-18 22:07:53 +0200

    uploading

--- increment.py
+++ increment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python 
 
-with open("test.txt", "r") as file:
+with open("/home/ubuntu/group4-2/test.txt", "r") as file:
     number = file.read()
     print(f"incoming number: {number}")
     new_number = int(number) + 1
-with open("test.txt", "w") as file:
+with open("/home/ubuntu/group4-2/test.txt", "w") as file:
     file.write(str(new_number))
     print(f"outcoming number: {new_number}")
